9788c835816c09e7fbaddc1da57b240b4ffc3690
alexandra <momotav717@gmail.ru>
2023-08-06 09:42:57 +0600

    step_1: init project, add gendiff

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="gendiff",
+        description="Compares two configuration files and shows a difference.",
+    )
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    args = parser.parse_args()
+    print(parser, args)
+
+
+if __name__ == "main":
+    main()
