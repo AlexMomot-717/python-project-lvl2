53f7d0145d534dbd230eb9cbb0328d2fe2953321
alexandra <momotav717@gmail.ru>
2023-08-27 22:22:12 +0600

    feat(step_5): improve gendiff for yaml files parsing

---
+++ gendiff/file_operations.py
@@ -0,0 +1,25 @@
+import json
+from typing import Any, Dict
+
+import yaml
+from yaml.loader import SafeLoader
+
+
+def get_file_format(file_name: str) -> str:
+    if file_name.endswith(".json"):
+        extention = "json"
+    elif file_name.endswith(".yaml") or file_name.endswith(".yml"):
+        extention = "yaml"
+    else:
+        raise Exception("Unsupported file format!")
+    return extention
+
+
+def parse_file(file_name: str) -> Dict[str, Any]:
+    extension = get_file_format(file_name)
+    with open(file_name, "r") as f:
+        if extension == "json":
+            result = dict(json.load(f))
+        else:
+            result = dict(yaml.load(f, Loader=SafeLoader))
+    return result
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
-import json
+
+from gendiff.core import generate_diff
 
 
 def get_args_parser() -> argparse.Namespace:
@@ -18,35 +19,6 @@ def get_args_parser() -> argparse.Namespace:
     )
     args = parser.parse_args()
     return args
-
-
-def generate_diff(first_file: str, second_file: str) -> str:
-    with (
-        open(first_file, "r") as file1,
-        open(second_file, "r") as file2,
-    ):
-        f1 = json.load(file1)
-        f2 = json.load(file2)
-
-    keys1_set = set(f1.keys())
-    keys2_set = set(f2.keys())
-    sorted_list = sorted(list(keys1_set | keys2_set))
-    common_keys = keys1_set & keys2_set
-    missing_keys = keys1_set - common_keys
-
-    output_str = "{\n"
-    for b in sorted_list:
-        if b in common_keys and f2[b] == f1[b]:
-            output_str += f"      {b}: {f2[b]}\n"
-        elif b in common_keys and f2[b] != f1[b]:
-            output_str += f"    + {b}: {f2[b]}\n"
-            output_str += f"    - {b}: {f1[b]}\n"
-        elif b in missing_keys:
-            output_str += f"    - {b}: {f1[b]}\n"
-        else:
-            output_str += f"    + {b}: {f2[b]}\n"
-    output_str += "}"
-    return output_str.lower()
 
 
 def main() -> None:
