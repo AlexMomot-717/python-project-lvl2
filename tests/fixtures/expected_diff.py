4440cb23d5128792230c19eafa30f72b19a6eafa
Savin Timur <mr.svntmr@gmail.com>
2023-09-12 20:56:06 +0200

    fix(step-6): make 4/5 tests pass
    by fixing wrong expected diff
    
    Signed-off-by: Savin Timur <mr.svntmr@gmail.com>

--- tests/fixtures/expected_diff.py
+++ tests/fixtures/expected_diff.py
@@ -2,8 +2,8 @@ EXPECTED_DIFF = """{
     - follow: false
       host: hexlet.io
     - proxy: 123.234.53.22
-    - timeout: 20
-    + timeout: 50
+    - timeout: 50
+    + timeout: 20
     + verbose: true
 }"""
 
