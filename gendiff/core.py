3fcc7a0fb39001853a0d7dcf82d59988f1be7d33
alexandra <momotav717@gmail.ru>
2023-10-02 20:12:09 +0600

    feat(step-6-2): improve gendiff work with nested diff

--- gendiff/core.py
+++ gendiff/core.py
@@ -1,16 +1,13 @@
-from typing import Any
-
-from gendiff.diff import get_diff_view
 from gendiff.file_operations import parse_file
-from gendiff.formatting import get_diff_str
+from gendiff.formatting import get_diff_str, get_diff_str_nested
 
 
 def generate_diff(
     first_file: str, second_file: str, output_format: str = "stylish"
-) -> Any:
+) -> str:
     f1 = parse_file(first_file)
     f2 = parse_file(second_file)
     if output_format == "stylish":
-        return get_diff_view(f1, f2)
+        return get_diff_str_nested(f1, f2)
     else:
         return get_diff_str(f1, f2)
